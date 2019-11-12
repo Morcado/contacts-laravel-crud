18f658a5dfee32b0897acf5dc0e6e48d63dce2ef
morcado <gposcarr18@gmail.com>
2019-11-12 12:45:45 -0600

    secure asset for bootstrap

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,12 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel crud</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css')}}">
+    <link rel="stylesheet" href="{{ secure_asset('css/app.css')}}">
 </head>
 <body>
     <div class="container">
         @yield('main')
     </div>
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ secure_asset('js/app.js') }}"></script>
 </body>
 </html>
